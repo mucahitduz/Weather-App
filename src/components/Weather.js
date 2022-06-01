033ceeeedb61ceea1a0d4b76cc15d692bb4e897d
Mücahit DÜZ <codbreaker24@gmail.com>
2022-06-01 18:30:28 +0300

    Initial commit

---
+++ src/components/Weather.js
@@ -0,0 +1,45 @@
+import axios from "axios";
+import React, { useContext, useState } from "react";
+import WeatherContext from "../contexts/WeatherContext";
+
+const Weather = () => {
+  const { weather, setWeather } = useContext(WeatherContext);
+  const [city, setCity] = useState("");
+
+  const getWeather = (e) => {
+    const key = "1bcac83f621e71521a22e3aca7db6a3b";
+    if (e.key === "Enter") {
+      axios(
+        `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${key}&units=metric`
+      ).then((res) => setWeather(res.data));
+      setCity("");
+    }
+  };
+
+  return (
+    <div className="container">
+      <input
+        className="input"
+        placeholder="Please enter a city"
+        onChange={(e) => setCity(e.target.value)}
+        value={city}
+        onKeyPress={getWeather}
+      />
+
+      {typeof weather.main === "undefined" ? (
+        <div className="weather-data">
+          <p>Welcome to weather app! Please enter a city</p>
+        </div>
+      ) : (
+        <div className="weather-data">
+          <p className="city"> {weather.name} </p>
+          <p className="temp"> {Math.round(weather.main.temp)}°C </p>
+          <p className="weather"> {weather.weather[0].main} </p>
+        </div>
+      )}
+      {weather.cod === "404" ? <p>City not found.</p> : <></>}
+    </div>
+  );
+};
+
+export default Weather;
---
+++ src/contexts/WeatherContext.js
@@ -0,0 +1,18 @@
+import { createContext, useState } from "react";
+
+const WeatherContext = createContext();
+
+export const WeatherProvider = ({ children }) => {
+  const [weather, setWeather] = useState([{}]);
+
+  const values = {
+    weather,
+    setWeather,
+  };
+
+  return (
+    <WeatherContext.Provider value={values}>{children}</WeatherContext.Provider>
+  );
+};
+
+export default WeatherContext;
